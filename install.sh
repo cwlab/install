f72c7994009bf385c42b4d186462de107c336ec0
Bram Gotink <bram.gotink@vtk.be>
2012-09-29 23:56:48 +0200

    calling the script itself doesn't work when piping through bash :(

--- install.sh
+++ install.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
 
-USER=${1:-root}
-
-if [[ $USER = 'root' ]]; then
-	if [ $EUID -eq 0 ]; then
-		echo "Don't run this script as root, it will ask for root permissions when needed." >&2
-		exit 1
-	fi
-	sudo $0 $(whoami) || exit $$
-	
-	. ~/.bashrc
-	exit 0
-else
-	if [ $EUID -eq 0 ]; then
-		echo -n ''
-	else
-		echo "Run this script without params" >&2
-		exit 1
-	fi
+if [ $EUID -eq 0 ]; then
+	echo "Don't run this script as root, it will ask for root permissions when needed." >&2
+	exit 1
 fi
 
 # check whether installation is already executed
-if [ -e /root/.installed ]; then
-    echo "The install script has already been run on this machine!" >&2
-    exit 1
-fi
+checkInstalled() {
+	if [ -e /root/.installed ]; then
+    	echo "The install script has already been run on this machine!" >&2
+    	exit 1
+	fi
+}
+
+sudo checkInstalled
 
 echo "Updating package repositories..." >&2
-apt-get update > /dev/null
+sudo aptitutde update > /dev/null
 
 echo "Updating software..."
-apt-get upgrade -y > /dev/null
+sudo aptititude upgrade -y > /dev/null
 
 echo "Installing required software..." >&2
-wget -q -O- https://cwlab.github.com/install/packages | xargs apt-get install -y
+wget -q -O- https://cwlab.github.com/install/packages | xargs sudo aptitude install -y
 
 # Create a temporary directory
 mkdir /tmp/cwlab
@@ -44,11 +33,17 @@ git clone -q git://github.com/cwlab/install.git
 
 # Start copying files
 echo "Copying Skeleton files..." >&2
-cp -f install/etc/skel/.bashrc /etc/skel
+sudo cp -f install/etc/skel/.bashrc /etc/skel
 
 # Get the new /root/.bashrc file
-cat install/root/.bashrc > /root/.bashrc
-[ -x /root/.bashrc ] || chmod +x /root/.bashrc
+rootBashrc() {
+	cat install/root/.bashrc > /root/.bashrc
+	[ -x /root/.bashrc ] || chmod +x /root/.bashrc
+}
+
+sudo rootBashrc
 
 # Get the new .bashrc file in the homedir of our current user
-su $USER -c 'cat /etc/skel/.bashrc > ~/.bashrc'
+cat /etc/skel/.bashrc > ~/.bashrc
+. ~/.bashrc
+
