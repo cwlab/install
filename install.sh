dc169fef87ce57f34f566e7ad07273d23d0a8865
Michiel Staessen <staessenmichiel@gmail.com>
2012-09-29 20:20:27 +0200

    Make it standalone

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit -1
+    echo "This script must be run as root" 1>&2
+    exit -1
 fi
 
-SOURCE=`dirname $0`
+# check whether installation is already executed
+if [ -e /etc/cwlab/ ]; then
+    echo "The CWLab install script has already been run on this machine!"
+    exit 1
+fi
 
 echo "Updating package repositories..."
 apt-get update > /dev/null
@@ -14,7 +18,14 @@ echo "Updating software..."
 apt-get dist-upgrade -y > /dev/null
 
 echo "Installing required software..."
-apt-get install -y git 
+apt-get install -y git
 
+# Create a temporary directory
+mkdir /tmp/cwlab && cd /tmp/cwlab
+
+# Get the configuration files
+git checkout https://github.com/cwlab/install.git install
+
+# Start copying files
 echo "Copying Skeleton files..."
-cp -R $SOURCE"/etc/skel" /etc
+cp -R "etc/skel" /etc
