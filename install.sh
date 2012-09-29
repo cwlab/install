dde50e8ac827f7840607ef8dd11f7cc07dab41c3
Bram Gotink <bram.gotink@vtk.be>
2012-09-30 00:21:17 +0200

    fix?

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 
 # check whether installation is already executed
 
-if [ sudo stat /root/.installed ]; then
+if [[ sudo stat /root/.installed ]]; then
    	echo "The install script has already been run on this machine!" >&2
    	exit 1
 fi
